64a95437f2ffca85c8bcdc6ed804d5e853254f2a
Akshat <akakkar_be23@thapar.edu>
2025-11-07 19:07:07 +0530

    op

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,23 +3,20 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import Home from './pages/Home.jsx'
-import Collection from './pages/Collection.jsx'
-import About from './pages/About.jsx'
-import Contact from './pages/Contact.jsx'
+import Login from './pages/login.jsx'
+import Signup from './pages/signup.jsx'
 
 const App = () => {
   return (
-       <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <div>
       <Navbar />
 
       <Routes>    
         <Route path='/' element = {<Home/>} />
-        <Route path='/collection' element={<Collection />}/>
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        
-        
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
+      
       <Footer /> 
       </div>
   )
